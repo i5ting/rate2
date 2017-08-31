3e15a2aee464e568058ccbb118ce69db5e2a1f4a
sangshilong <sangshilong@luojilab.com>
2017-09-01 03:49:57 +0800

    refact, please ignore

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 
 var rate = require('.')(require('./config'))
 
+var i = 0
+
 app.get('/', rate, function (req, res) {
-    res.status(200).json({ name: 'tobi' });
+    i++
+    res.status(200).json({ count: i });
 });
 
 app.listen(3000)
