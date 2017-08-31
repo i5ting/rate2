3f9643a8282e500546d1d89ee01beaf718f356d2
sangshilong <sangshilong@luojilab.com>
2017-09-01 03:45:44 +0800

    refact, please ignore

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-var rate = require('.')
+var rate = require('.')(require('./config'))
 
 app.get('/', rate, function (req, res) {
     res.status(200).json({ name: 'tobi' });
--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 var Redis = require('ioredis');
 
 
-var all_count = require('./config').count
-var config = require('./config').conn
-var key = require('./config').key
-var redirect_url = require('./config').redirect_url
-
-var redisClient = []
-
-var count = all_count / config.length 
-for(var i in config) {
-    var conn = config[i].split(':')
-    console.log(conn)
-
-    var redis = new Redis(conn[1], conn[0]);
-    redisClient.push(redis)
-} 
-
-var n_requrest = 0;//请求总数
-var m_redis = redisClient.length;// redis个数
-
 function deleteOne (arr, item) {
     arr.splice(
         arr.indexOf(item), 1
@@ -27,30 +8,55 @@ function deleteOne (arr, item) {
     return arr 
 }
 
-module.exports = function(req, res, next) {
-    n_requrest++;
-    var i_redis = n_requrest%m_redis;
+module.exports = function(cfg) {
 
-    var c_client = redisClient[i_redis] //获取第i个redis连接
-    var conn = config[i_redis].split(':')
-    console.log(conn)
- 
+    var all_count = cfg.count
+    var config = cfg.conn
+    var key = cfg.key
+    var redirect_url = cfg.redirect_url
 
-    c_client.exists(key).then(function(isKeyExists){
-        if (isKeyExists === 1) {
-            // 如果存在，则blpop
-            c_client.blpop(key, 100).then(function(res){
-                console.log(conn + " - " + res)
-                next()
-            }).catch(function(err) {
-                next()
-            })
-        }else {
-            // 如果key不存在，则移除redis链接
-            // deleteOne(redisClient, c_client)
+    var redisClient = []
+    
+    var count = all_count / cfg.conn.length 
+    for(var i in config) {
+        var conn = config[i].split(':')
+        console.log(conn)
+    
+        var redis = new Redis(conn[1], conn[0]);
+        redisClient.push(redis)
+    } 
+    
+    var n_requrest = 0;//请求总数
+    var m_redis = redisClient.length;// redis个数
+    
 
-            // 重定向
-            res.redirect(redirect_url)
-        }
-    })
+    return function(req, res, next) {
+    
+        n_requrest++;
+        var i_redis = n_requrest%m_redis;
+    
+        var c_client = redisClient[i_redis] //获取第i个redis连接
+        var conn = config[i_redis].split(':')
+        console.log(conn)
+     
+    
+        c_client.exists(key).then(function(isKeyExists){
+            if (isKeyExists === 1) {
+                // 如果存在，则blpop
+                c_client.blpop(key, 100).then(function(res){
+                    console.log(conn + " - " + res)
+                    next()
+                }).catch(function(err) {
+                    next()
+                })
+            }else {
+                // 如果key不存在，则移除redis链接
+                // deleteOne(redisClient, c_client)
+    
+                // 重定向
+                res.redirect(redirect_url)
+            }
+        })
+    }
+    
 }
